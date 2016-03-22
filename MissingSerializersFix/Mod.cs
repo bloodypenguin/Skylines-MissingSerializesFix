d4e077e3aad94eec371b1c8634f7d84fe44aa002
Egor Aralov <aei.omsk@gmail.com>
2016-03-22 22:06:01 +0600

    Disable mod functionality

--- MissingSerializersFix/Mod.cs
+++ MissingSerializersFix/Mod.cs
@@ -4,7 +4,7 @@ namespace MissingSerializersFix
 {
     public class Mod : IUserMod
     {
-        public string Name => "Missing Serializers/Deserializers Fix";
-        public string Description => "Makes possible to use AD Airport and International Airport templates";
+        public string Name => "Missing Serializers/Deserializers Fix [DEPRECATED]";
+        public string Description => "This mod is no longer needed. It's a part of vanilla game now.";
     }
 }
