36765e8d2bc5ebec8ea60ae2dfb0954359c6e3da
earalov <earalov@thumbtack.net>
2016-11-10 13:59:52 +0600

    Move stub names to constants

--- MissingSerializersFix/PackageHelperDetour.cs
+++ MissingSerializersFix/PackageHelperDetour.cs
@@ -7,6 +7,9 @@ namespace MissingSerializersFix
 {
     public class PackageHelperDetour
     {
+        public const string STUB_TREE = "tree01";
+        public const string STUB_PROP = "Door Marker";
+        public const string STUB_SUB_BULDING = "Metro Entrance";
         private static RedirectCallsState state;
         private static IntPtr _intPtr;
         private static IntPtr _intPtr2;
@@ -86,7 +89,7 @@ namespace MissingSerializersFix
                 subBuildings[mainBuildingId].Add(buildingId);
                 return (object)new BuildingInfo.SubInfo()
                 {
-                    m_buildingInfo = PrefabCollection<BuildingInfo>.FindLoaded("Metro Entrance"), //a fake sub building to prevent exception
+                    m_buildingInfo = PrefabCollection<BuildingInfo>.FindLoaded(STUB_SUB_BULDING), //a fake sub building to prevent exception
                     m_position = r.ReadVector3(),
                     m_angle = r.ReadSingle(),
                     m_fixedHeight = r.ReadBoolean()
@@ -101,7 +104,7 @@ namespace MissingSerializersFix
                     propVariations.Add(mainPropId, new List<string>());
                 }
                 propVariations[mainPropId].Add(propId);
-                var stubProp = PrefabCollection<PropInfo>.FindLoaded("Door Marker"); //a fake prop to prevent exception
+                var stubProp = PrefabCollection<PropInfo>.FindLoaded(STUB_PROP); //a fake prop to prevent exception
                 return new PropInfo.Variation()
                 {
                     m_prop = stubProp,
@@ -118,7 +121,7 @@ namespace MissingSerializersFix
                     treeVariations.Add(mainTreeId, new List<string>());
                 }
                 treeVariations[mainTreeId].Add(treeId);
-                var stubTree = PrefabCollection<TreeInfo>.FindLoaded("tree01"); //a fake tree to prevent exception
+                var stubTree = PrefabCollection<TreeInfo>.FindLoaded(STUB_TREE); //a fake tree to prevent exception
                 return new TreeInfo.Variation()
                 {
                     m_tree = stubTree,
